f4b91bfb152404ef7e70752527517f8a10336817
AminatRazaq <aminatrazaq@yahoo.com>
2023-01-24 09:59:46 -0800

    main.h

---
+++ main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
+
+#define BUFF_SIZE 1024
+int _printf(const char *format, ...);
+void print_buffer(char buffer[], int *buff_ind);
+#endif/*MAIN_H*/
