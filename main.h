eeb6d93d8fc7128d391dd56da065120a7348f225
AminatRazaq <aminatrazaq@yahoo.com>
2023-01-24 15:02:25 -0800

    task 0

--- main.h
+++ main.h
@@ -6,6 +6,7 @@
 #include <unistd.h>
 
 #define BUFF_SIZE 1024
+int _strlen(char *s);
 void print_int_number(int);
 int _printf(const char *format, ...);
 void print_buffer(char buffer[], int *buff_ind);
