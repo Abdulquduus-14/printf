603fa6856795acf440b85d4b1c07e692a06e532b
Amidu Qudus <qudusamidu14@gmail.com>
2023-01-25 08:41:34 -0800

    main.h file checked

--- main.h
+++ main.h
@@ -6,8 +6,9 @@
 #include <unistd.h>
 
 #define BUFF_SIZE 1024
+
 int _strlen(char *s);
-void print_int_number(int);
+void print_int_number(int n);
 int _printf(const char *format, ...);
 void print_buffer(char buffer[], int *buff_ind);
 #endif/*MAIN_H*/
